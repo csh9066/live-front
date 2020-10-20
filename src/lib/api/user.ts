a9b583748d2fabc71ccdcf514c7b22c685a11078
seungho <50465901+csh9066@users.noreply.github.com>
2020-10-20 20:48:10 +0900

    Update user api

--- src/lib/api/user.ts
+++ src/lib/api/user.ts
@@ -11,3 +11,13 @@ export const createUserRequest = async (registerForm: RegisterFormType) => {
   const response = await client.post('/user', registerForm);
   return response;
 };
+
+export const checkUserRequest = async () => {
+  const response = await client.get('/user/check');
+  return response;
+};
+
+export const logoutRequest = async () => {
+  const response = await client.get('/user/logout');
+  return response;
+};
