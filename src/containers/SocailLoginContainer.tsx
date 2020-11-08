096505932a3d9c33b8bc3a1825d46bfdb66a587f
seungho <50465901+csh9066@users.noreply.github.com>
2020-11-08 16:03:44 +0900

    Remove components

--- src/containers/SocailLoginContainer.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-type SocailLoginContainerProps = {};
-
-function SocailLoginContainer(props: SocailLoginContainerProps) {
-  return <div></div>;
-}
-
-export default SocailLoginContainer;
