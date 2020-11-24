3179d556b69db8f7e0a89d4170fa5930b121def5
seungho <50465901+csh9066@users.noreply.github.com>
2020-11-24 17:25:55 +0900

    Refactroy socket

---
+++ src/socket.ts
@@ -0,0 +1,17 @@
+import { io } from 'socket.io-client';
+
+export const SocketEvent = {
+  ONLINE: 'ONLINE' as const,
+  DM: 'DM' as const,
+  CHANNEL_CHAT: 'CHANNEL_CHAT' as const,
+  JOIN_CHANNELS: 'JOIN_CHANNELS' as const,
+  JOIN_CHANNEL: 'JOIN_CHANNEL' as const,
+  ADD_CHANNEL_MEMBERS: 'ADD_CHANNEL_MEMBERS' as const,
+  ADD_CHANNEL: 'ADD_CHANNEL' as const,
+};
+
+const socket = io(`${process.env.REACT_APP_SERVER_PORT}`, {
+  transports: ['websocket'],
+});
+
+export default socket;
